35ffcdd02f38b9326703d000936706e6d3adc611
xVistyx <wiktor.hc@gmail.com>
2022-07-18 15:13:24 +0200

    js-dodaj-usun-tekst

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let buttondodaj = document.getElementById('dodaj').onclick = function() {
+
+    miejsceTekstu.textContent = 'Dodano paragraf!'
+}
+let buttonusun = document.getElementById('usun').onclick = function() {
+
+    miejsceTekstu.textContent = ''
+}
+let miejsceTekstu = document.querySelector('p')
+
+
+
+
+
+
